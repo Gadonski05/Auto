f6974ef15044711073f425a16a95ad626a948234
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-05-08 21:26:10 -0300

    Todas as variáveis da classe moto
    Criação da classe moto, com extends da classe Veiculo para puxar as infos principais.

--- Moto.java
+++ Moto.java
@@ -1,3 +1,49 @@
-public class Moto {
+public class Moto extends Veiculo{
+
+    public static final String Virar = null;
+    private int Cilindradas;
+    private String tipoCombustivel;
+    private String tipoFreio;
+
+    public Moto(int Cilindradas, String tipoCombustivel, String tipoFreio){
+
+        this.Cilindradas = Cilindradas;
+        this.tipoCombustivel = tipoCombustivel;
+        this.tipoFreio = tipoFreio;
+    }
     
+    public int getCilindadas(){
+
+        return Cilindradas;
+    }
+
+    public String getTipocombustivel(){
+
+        return tipoCombustivel;
+    }
+
+    public String getTipofreio(){
+
+        return tipoFreio;
+    }
+
+    public void Mot(){
+
+        System.out.println("Subindo na moto!!");
+    }
+
+    public void Ligarmoto(){
+
+        System.out.println("Ligando a moto!!");
+    }
+
+    public void Desligarmoto(){
+
+        System.out.println("Desligando a moto!!");
+    }
+
+    public void Mdesportivo(){
+
+        System.out.println("Ronco mudou!!");
+    }
 }

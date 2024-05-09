3038fbacc3e43ef04ee6d62eefce113a88fd568e
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-05-08 21:27:11 -0300

    Update
    Reformulação da classe Onibus.

--- Onibus.java
+++ Onibus.java
@@ -1,3 +1,39 @@
-public class Onibus {
-    
+public class Onibus extends Veiculo{
+
+    int CapaPss;
+    int Portas;
+    int Nparadas;
+
+    public Onibus(int CapaPss, int Portas, int Nparadas){
+
+        this.CapaPss = CapaPss;
+        this.Portas = Portas;
+        this.Nparadas = Nparadas;
+
+    }
+
+    public int getCapaPss(){
+
+        return CapaPss;
+    }
+
+    public int getPortas(){
+
+        return Portas;
+    }
+
+    public int getNparadas(){
+
+        return Nparadas;
+    }
+
+    public void Entrar(){
+
+        System.out.println("Entrando no onibus!!");
+    }
+
+    public void Sair(){
+
+        System.out.println("Saindo do onibus!!");
+    }
 }

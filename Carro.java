d8033967959eb7d7b7b2ab17f816247439899028
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-05-29 20:10:15 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,21 +1,15 @@
 public class Carro extends Veiculo{
 
+    private static boolean ligar;
+    private String tipoTransmissao;
+    private int numPortas;
+    private float portaMalas;
+
     public Carro(String modelo, int ano, String cor, int numPortas, String tipoTransmissao, float portaMalas){
-        super(modelo, ano, cor);
+        super(modelo, ano, cor, ligar);
 
         this.tipoTransmissao = tipoTransmissao;
         this.numPortas = numPortas;
-        this.portaMalas = portaMalas;
-    }
-    
-    private int numPortas;
-    private String tipoTransmissao;
-    private float portaMalas;
-    
-    public Carro(int numPortas, String tipoTransmissao, float portaMalas){
-        
-        this.numPortas = numPortas;
-        this.tipoTransmissao = tipoTransmissao;
         this.portaMalas = portaMalas;
     }
 
@@ -24,14 +18,28 @@ public class Carro extends Veiculo{
         return numPortas;
     }
 
+    public void setNumportas(int numPortas){
+
+        this.numPortas = numPortas;
+    }
+
     public String getTipotransmissao(){
 
         return tipoTransmissao;
     }
 
+    public void setTipotrasmissao(String tipoTransmissao){
+
+        this.tipoTransmissao = tipoTransmissao;
+    }
+
     public float getPoratamalas(){
 
         return portaMalas;
+    }
+    public void setPortamalas(float portaMalas){
+
+        this.portaMalas = portaMalas;
     }
 
     public void Ligarfarol(){
@@ -47,6 +55,11 @@ public class Carro extends Veiculo{
     public void Fechandoteto(){
 
         System.out.println("Fechando teto!!");
+    }
+
+    public void Freiando() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Freiando'");
     }
 }
 

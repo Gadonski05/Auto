8e30421cd2df708d310938485a8fba1e8e9d97b7
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-05-29 20:10:25 -0300

    Update Onibus.java

--- Onibus.java
+++ Onibus.java
@@ -1,10 +1,11 @@
 public class Onibus extends Veiculo{
 
-    int CapaPss;
-    int Portas;
-    int Nparadas;
+    private int CapaPss;
+    private int Portas;
+    private int Nparadas;
 
     public Onibus(int CapaPss, int Portas, int Nparadas){
+        super(cor, Nparadas, cor, ligar);
 
         this.CapaPss = CapaPss;
         this.Portas = Portas;
@@ -27,6 +28,21 @@ public class Onibus extends Veiculo{
         return Nparadas;
     }
 
+    public void setCapaPss(int capaPss) {
+
+        CapaPss = capaPss;
+    }
+
+    public void setPortas(int portas) {
+
+        Portas = portas;
+    }
+
+    public void setNparadas(int nparadas) {
+
+        Nparadas = nparadas;
+    }
+
     public void Entrar(){
 
         System.out.println("Entrando no onibus!!");
@@ -35,5 +51,30 @@ public class Onibus extends Veiculo{
     public void Sair(){
 
         System.out.println("Saindo do onibus!!");
+    }
+
+    public void Freiando() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Freiando'");
+    }
+
+    public void Virar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Virar'");
+    }
+
+    public void CapaPss() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'CapaPss'");
+    }
+
+    public void Nparadas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Nparadas'");
+    }
+
+    public void Portas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Portas'");
     }
 }

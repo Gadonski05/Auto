398d4f67187aa3c640fe28a5e2090ec557f9276d
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-05-29 20:10:12 -0300

    Update Bicicleta.java

--- Bicicleta.java
+++ Bicicleta.java
@@ -3,10 +3,9 @@ public class Bicicleta extends Veiculo {
     private int numeroMarchas;
     private  String tipoFreio;
     private String estiloQuadro;
-    
-}
 
 public Bicicleta(int numeroMarchas, String tipoFreio, String estiloQuadro){
+    super(tipoFreio, numeroMarchas, estiloQuadro, ligar);
 
     this.numeroMarchas = numeroMarchas;
     this.tipoFreio = tipoFreio;
@@ -18,9 +17,19 @@ public int getNumeromarchas(){
     return numeroMarchas;
 }
 
+public void setNumeromarchas(int numeroMarchas){
+
+    this.numeroMarchas = numeroMarchas;
+}
+
 public String getTipofreio(){
 
     return tipoFreio;
+}
+
+public void setTipofreio(String tipoFreio){
+
+    this.tipoFreio = tipoFreio;
 }
 
 public String getEstiloquadro(){
@@ -28,7 +37,12 @@ public String getEstiloquadro(){
     return estiloQuadro;
 }
 
-public void Bicicleta(){
+public void setEstiloquadro(String estiloQuadro){
+
+    this.estiloQuadro = estiloQuadro;
+}
+
+public void bicicleta(){
 
     System.out.println("Subindo na bike!!");
 }
@@ -41,4 +55,7 @@ public void Pedalar(){
 public void Parar(){
 
     System.out.println("Parando a bicicleta!!");
+
+}
+
 }
